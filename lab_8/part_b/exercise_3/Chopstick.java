f3ba8cf62c0d3a137d6f5e6a51ccf353bf461c1a
NikhilPeri <nperi104@uottawa.ca>
2019-04-01 17:31:50 -0400

    and that is q3

--- lab_8/part_b/exercise_3/Chopstick.java
+++ lab_8/part_b/exercise_3/Chopstick.java
@@ -1,21 +1,37 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 public class Chopstick {
 	private int ID;
-// hint: use a local variable to indicate whether the chopstick is free 
-//                        (lying on the table), e.g.  private boolean free;
+	private boolean free;
+	private Semaphore semaphore = new Semaphore(1);
+
+	final long maxWait = 420;
 
 	Chopstick(int ID) {
 		  this.ID = ID;
-	
+			this.free = true;
 	}
-	
-	synchronized void take() {
-	
+
+	synchronized boolean take() {
+		if (!this.free) {
+			try {
+				if (!this.semaphore.tryAcquire(maxWait, TimeUnit.MILLISECONDS)) {
+					return false;
+				}
+			} catch(InterruptedException e) {
+				return false;
+			}
+		}
+		this.free = false;
+		return true;
 	}
-	
+
 	synchronized void release() {
-	
+		this.free = true;
+		notify();
 	}
-	    
+
 	public int getID() {
 	    return(ID);
 	}
